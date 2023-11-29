5bd18cdd0e0d4a6364c534a870ce8d340a7dcf88
Karol Moroz <karol@moroz.dev>
2023-11-29 16:51:09 +0800

    Rename with F2

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -9,7 +9,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { noremap = true })
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
