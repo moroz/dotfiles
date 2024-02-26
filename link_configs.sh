f7470b6296d18d9e0e014643e529eaba274a4969
Karol Moroz <karol@moroz.dev>
2024-02-26 17:57:15 +0800

    Update link_configs script

--- link_configs.sh
+++ link_configs.sh
@@ -2,15 +2,15 @@
 
 mkdir -p $HOME/.config/fontconfig
 
-ln -sf $HOME/.dotfiles/nvim $HOME/.config/nvim
-[[ -f $HOME/.tmux.conf ]] || ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
-[[ -f $HOME/.prettierrc.js ]] || ln -s $HOME/.dotfiles/.prettierrc.js $HOME/.prettierrc.js
-[[ -f $HOME/.eslintrc.js ]] || ln -s $HOME/.dotfiles/.eslintrc.js $HOME/.eslintrc.js
-[[ -f $HOME/.spacemacs ]] || ln -s $HOME/.dotfiles/.spacemacs $HOME/.spacemacs
+[[ -L $HOME/.config/nvim ]] || ls -s $HOME/.dotfiles/nvim $HOME/.config/nvim
+[[ -L $HOME/.tmux.conf ]] || ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+[[ -L $HOME/.prettierrc.js ]] || ln -s $HOME/.dotfiles/.prettierrc.js $HOME/.prettierrc.js
+[[ -L $HOME/.eslintrc.js ]] || ln -s $HOME/.dotfiles/.eslintrc.js $HOME/.eslintrc.js
+[[ -L $HOME/.spacemacs ]] || ln -s $HOME/.dotfiles/.spacemacs $HOME/.spacemacs
 [[ -f $HOME/.zshrc ]] || echo ". ~/.dotfiles/.zshrc" > ~/.zshrc
-[[ -f $HOME/.psqlrc ]] || ln -s $HOME/.dotfiles/.psqlrc $HOME/.psqlrc
-[[ -f $HOME/.gitignore ]] || ln -s $HOME/.dotfiles/.gitignore $HOME/.gitignore
-[[ -f $HOME/.config/fontconfig/fonts.conf ]] || ln -s $HOME/.dotfiles/fonts.conf $HOME/.config/fontconfig/fonts.conf
+[[ -L $HOME/.psqlrc ]] || ln -s $HOME/.dotfiles/.psqlrc $HOME/.psqlrc
+[[ -L $HOME/.gitignore ]] || ln -s $HOME/.dotfiles/.gitignore $HOME/.gitignore
+[[ -L $HOME/.config/fontconfig/fonts.conf ]] || ln -s $HOME/.dotfiles/fonts.conf $HOME/.config/fontconfig/fonts.conf
 git config --global core.excludesfile ~/.gitignore
 
 mkdir -p ~/.ctags.d
@@ -18,12 +18,8 @@ mkdir -p ~/.ctags.d
 
 [[ -d $HOME/.doom.d ]] || ln -s $HOME/.dotfiles/.doom.d $HOME/.doom.d
 
-os="`uname`"
-if [[ "$os" == 'Darwin' ]]; then
-  mkdir -p "$HOME/Library/Application Support/Code/User"
-  ln -sf $HOME/.dotfiles/Code/User/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
-  ln -sf $HOME/.dotfiles/Code/User/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings.json"
-  if [ -x $(which brew) ]; then
+if [[ "$(uname)" == 'Darwin' ]]; then
+  if which brew >/dev/null; then
     brew install zsh tmux reattach-to-user-namespace mc ag neovim wget curl postgresql python3 nodejs direnv aws-vault ripgrep universal-ctags
     brew install --cask firefox google-chrome iterm2 spectacle karabiner-elements signal slack tableplus
     sudo echo $(which zsh) >> /etc/shells
@@ -34,8 +30,5 @@ if [[ "$os" == 'Darwin' ]]; then
   defaults write -g ApplePressAndHoldEnabled -bool false
   mkdir -p $HOME/Pictures/screenshots
   defaults write com.apple.screencapture location $HOME/Pictures/screenshots
-elif [[ "$os" == 'Linux' ]]; then
-  mkdir -p $HOME/.config/Code/User/
-  ln -sf $HOME/.dotfiles/Code/User/settings.json $HOME/.config/Code/User/settings.json
-  ln -sf $HOME/.dotfiles/Code/User/keybindings.json $HOME/.config/Code/User/keybindings.json
+  [[ -L $HOME/.config/alacritty ]] || ln -s $HOME/.dotfiles/alacritty $HOME/.config/alacritty
 fi
