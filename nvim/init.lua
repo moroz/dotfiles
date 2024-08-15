b21ce35f8fee9eb10bfdc622153f9a85340eb260
Karol Moroz <karol@moroz.dev>
2024-08-15 17:49:50 +0800

    Install leetcode.nvim, update bundle

--- nvim/init.lua
+++ nvim/init.lua
@@ -224,6 +224,24 @@ require('lazy').setup({
       'NvimTreeOpen',
     },
     config = true,
+  },
+
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      lang = "golang"
+    },
   }
 }, {})
 
