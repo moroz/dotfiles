d8e22b48cd56908f4d9e4d87261cb62fc74b9498
Karol Moroz <karol@moroz.dev>
2026-03-22 16:04:31 +0100

    Start treesitter on load for some files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1035,5 +1035,12 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'go', 'ruby', 'elixir', 'c' },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
