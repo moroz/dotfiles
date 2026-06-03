0143807b6a1aa13183cba9f0c4d3544a47be4c33
Karol Moroz <karol@moroz.dev>
2026-06-03 21:08:53 +0200

    Install gutentags

--- nvim/init.lua
+++ nvim/init.lua
@@ -825,6 +825,7 @@ require('lazy').setup({
       }
     end,
   },
+  'ludovicchabant/vim-gutentags',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
