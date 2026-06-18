023038ca2a739a3b90100b424ed6d75b386ff51d
Karol Moroz <karol@moroz.dev>
2026-06-18 13:57:49 +0200

    Svelte syntax highlighting support

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'evanleck/vim-svelte',
+    branch = 'main',
+    ft = 'svelte',
+    dependencies = {
+      'othree/html5.vim',
+      'pangloss/vim-javascript',
+    },
+  },
+}
